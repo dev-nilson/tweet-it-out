37fa4da7de6e514d8aa5ddcc84b9c064ed185dfa
Denilson <dlemus@harding.edu>
2023-03-16 13:47:49 -0600

    🚧 Work on feed component

---
+++ src/components/Feed/Feed.tsx
@@ -0,0 +1,14 @@
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+function Feed() {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold ml-5 text-lg">Home</h1>
+        <ArrowPathIcon className="h-7 w-7 mr-5 cursor-pointer transition-all duration-300 ease-out hover:rotate-180 active:scale-110" />
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
