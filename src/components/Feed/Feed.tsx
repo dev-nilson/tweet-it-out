83515ebd72146dda2e545b5c405d0b5cd79921a9
Denilson <dlemus@harding.edu>
2023-03-17 15:55:14 -0600

    💄 Update styles

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,12 +1,14 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import TweetBox from "../TweetBox/TweetBox";
 
 function Feed() {
   return (
     <div className="lg:col-span-5 col-span-7 pt-3">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between pb-3">
         <h1 className="font-bold ml-5 text-lg">Home</h1>
         <ArrowPathIcon className="h-7 w-7  mr-5 cursor-pointer transition-all duration-300 ease-out hover:rotate-180 active:scale-110" />
       </div>
+      <TweetBox />
     </div>
   );
 }
--- src/components/TweetBox/TweetBox.tsx
+++ src/components/TweetBox/TweetBox.tsx
@@ -7,9 +7,9 @@ import {
 
 function TweetBox() {
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="What's happening?" />
+    <div className="flex items-center flex-1 px-5 border-y">
+      <form className="flex flex-1 flex-col my-3">
+        <input className="outline-none my-3 text-lg" type="text" placeholder="What's happening?" />
         <div className="flex items-center">
           <div className="flex space-x-2 text-blue-400 flex-1">
             <PhotoIcon className="h-5 w-5" />
