c57e07e50eb7970203af51b48132d681050bd6dc
Denilson <dlemus@harding.edu>
2023-03-17 16:09:44 -0600

    💄 Update tweet box styles

--- src/components/TweetBox/TweetBox.tsx
+++ src/components/TweetBox/TweetBox.tsx
@@ -9,13 +9,25 @@ function TweetBox() {
   return (
     <div className="flex items-center flex-1 px-5 border-y">
       <form className="flex flex-1 flex-col my-3">
-        <input className="outline-none my-3 text-lg" type="text" placeholder="What's happening?" />
+        <input
+          className="outline-none my-3 text-lg"
+          type="text"
+          placeholder="What's happening?"
+        />
         <div className="flex items-center">
-          <div className="flex space-x-2 text-blue-400 flex-1">
-            <PhotoIcon className="h-5 w-5" />
-            <FaceSmileIcon className="h-5 w-5" />
-            <CalendarIcon className="h-5 w-5" />
-            <MapPinIcon className="h-5 w-5" />
+          <div className="flex text-blue-400 flex-1">
+            <div className="clickable">
+              <PhotoIcon className="h-5 w-5" />
+            </div>
+            <div className="clickable">
+              <FaceSmileIcon className="h-5 w-5" />
+            </div>
+            <div className="clickable">
+              <CalendarIcon className="h-5 w-5" />
+            </div>
+            <div className="clickable">
+              <MapPinIcon className="h-5 w-5" />
+            </div>
           </div>
           <button className="bg-blue-400 font-semibold text-white px-5 py-1 rounded-full">
             Tweet
