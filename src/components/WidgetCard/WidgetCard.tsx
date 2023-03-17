eb96acd215b791426e2dd43b5b33eea905a639db
Denilson <dlemus@harding.edu>
2023-03-17 16:39:55 -0600

    ✨ Add widget cards component

--- src/components/WidgetCard/WidgetCard.tsx
+++ src/components/WidgetCard/WidgetCard.tsx
@@ -1,11 +1,13 @@
 type WidgetCardProps = {
   title: string;
+  children: any;
 };
 
-function WidgetCard({ title }: WidgetCardProps) {
+function WidgetCard({ title, children }: WidgetCardProps) {
   return (
-    <div>
+    <div className="mx-1 my-4 p-3 w-full bg-gray-100 rounded-xl">
       <h1 className="text-lg font-bold">{title}</h1>
+      {children}
     </div>
   );
 }
--- src/components/Widgets/Widgets.tsx
+++ src/components/Widgets/Widgets.tsx
@@ -6,13 +6,22 @@ function Widgets() {
     <div className="col-span-2 hidden lg:inline border-l px-2">
       <div className="flex items-center space-x-2 bg-gray-100 p-2 rounded-full mt-2">
         <MagnifyingGlassIcon className="w-5 h-5" />
-        <input className="bg-transparent flex-1 outline-none" type="text" placeholder="Search" />
+        <input
+          className="bg-transparent flex-1 outline-none"
+          type="text"
+          placeholder="Search"
+        />
       </div>
       <div>
-        <WidgetCard title="Trends for you" />
+        <WidgetCard title="Trends for you">
+          Placeholder
+        </WidgetCard>
+        <WidgetCard title="Who to follow">
+          Placeholder
+        </WidgetCard>
       </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
