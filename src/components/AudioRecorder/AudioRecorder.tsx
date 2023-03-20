81106a0cd0be8e68557f5f093f4f00d103c35586
Denilson <dlemus@harding.edu>
2023-03-20 17:54:03 -0600

    ✨ Add functionality to start recording

--- src/components/AudioRecorder/AudioRecorder.tsx
+++ src/components/AudioRecorder/AudioRecorder.tsx
@@ -3,6 +3,7 @@ import { MicrophoneIcon, PauseIcon } from "@heroicons/react/24/outline";
 
 const AudioRecorder = () => {
   const [audio, setAudio] = useState<any>(null);
+  const [stream, setStream] = useState<any>(null);
   const [isSupported, setIsSupported] = useState(true);
   const [permission, setPermission] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
@@ -13,11 +14,15 @@ const AudioRecorder = () => {
         setIsSupported(false);
 
       try {
-        await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: false,
-        });
-        setPermission(true);
+        await navigator.mediaDevices
+          .getUserMedia({
+            audio: true,
+            video: false,
+          })
+          .then((res) => {
+            setStream(res);
+            setPermission(true);
+          });
       } catch (err) {
         setPermission(false);
       }
@@ -26,12 +31,26 @@ const AudioRecorder = () => {
     getMicrophone();
   }, []);
 
+  const startRecording = () => {
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+    setIsRecording(true);
+
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+  };
+  
+  const stopRecording = () => {};
+  
   return (
     <div className="flex items-center space-x-1">
       {isRecording ? (
-        <PauseIcon className="h-12 -ml-3 clickable" />
+        <PauseIcon className="h-12 -ml-3 clickable" onClick={stopRecording} />
       ) : (
-        <MicrophoneIcon className="h-12 -ml-3 clickable" />
+        <MicrophoneIcon
+          className="h-12 -ml-3 clickable"
+          onClick={startRecording}
+        />
       )}
       <div>
         {!isSupported && (
