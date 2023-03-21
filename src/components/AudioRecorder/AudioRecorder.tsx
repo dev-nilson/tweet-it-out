0045785770910163f4ec13c337734385b7815fa5
Denilson <dlemus@harding.edu>
2023-03-20 18:56:36 -0600

    🐛 Fix recording functions

--- src/components/AudioRecorder/AudioRecorder.tsx
+++ src/components/AudioRecorder/AudioRecorder.tsx
@@ -3,7 +3,9 @@ import { MicrophoneIcon, PauseIcon } from "@heroicons/react/24/outline";
 
 const AudioRecorder = () => {
   const mediaRecorder = useRef<any>(null);
+  const mimeType = "audio/webm";
   const [audio, setAudio] = useState<any>(null);
+  const [chunks, setChunks] = useState<any>([]);
   const [stream, setStream] = useState<any>(null);
   const [isSupported, setIsSupported] = useState(true);
   const [permission, setPermission] = useState(false);
@@ -18,7 +20,6 @@ const AudioRecorder = () => {
         await navigator.mediaDevices
           .getUserMedia({
             audio: true,
-            video: false,
           })
           .then((res) => {
             setStream(res);
@@ -33,23 +34,38 @@ const AudioRecorder = () => {
   }, []);
 
   const startRecording = () => {
-    const media = new MediaRecorder(stream);
+    setIsRecording(true);
+
+    const media = new MediaRecorder(stream, { type: mimeType });
     mediaRecorder.current = media;
     mediaRecorder.current.start();
-    setIsRecording(true);
+    let audioChunks = [];
+    mediaRecorder.current.ondataavailable = (e) => {
+      if (typeof e.data === "undefined") return;
+      if (e.data.size === 0) return;
+      audioChunks.push(e.data);
+    };
+    setChunks(audioChunks);
 
     console.log(mediaRecorder.current.state);
     console.log("recorder started");
   };
-  
+
   const stopRecording = () => {
-    mediaRecorder.current.stop();
     setIsRecording(false);
+
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(chunks, { type: mimeType });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudio(audioUrl);
+      setChunks([]);
+    };
 
     console.log(mediaRecorder.current.state);
     console.log("recorder stopped");
   };
-  
+
   return (
     <div className="flex items-center space-x-1">
       {isRecording ? (
