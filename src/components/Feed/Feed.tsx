556dbdfbb280e676653b03eeba072da9b56f4a93
Denilson <dlemus@harding.edu>
2023-04-06 15:23:09 -0600

    ✨ Add functionality to reload button

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -31,7 +31,9 @@ function Feed() {
     <div className="lg:col-span-5 col-span-7 pt-3">
       <div className="flex items-center justify-between pb-3">
         <h1 className="font-bold ml-5 text-lg">Home</h1>
-        <ArrowPathIcon className="h-7 w-7  mr-5 cursor-pointer transition-all duration-300 ease-out hover:rotate-180 active:scale-110" />
+        <div onClick={() => location.reload()}>
+          <ArrowPathIcon className="h-7 w-7  mr-5 cursor-pointer transition-all duration-300 ease-out hover:rotate-180 active:scale-110" />
+        </div>
       </div>
       <TweetBox />
       {audios.length > 0 ? (
