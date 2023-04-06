ba08247aa6d602e9b212c25ff84d837b6a504ab2
Denilson <dlemus@harding.edu>
2023-04-06 15:27:19 -0600

    ✨ Add functionality to like

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -7,12 +7,15 @@ import {
   ShareIcon,
 } from "@heroicons/react/24/outline";
 import avatar from "../../../assets/placeholder.png";
+import { useState } from "react";
 
 type TweetProps = {
   tweet: string;
 };
 
 function Tweet({ tweet }: TweetProps) {
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
     <div className="flex flex-col space-x-3 border-b pt-4 pb-1 px-4 hover:bg-gray-50 cursor-pointer">
       <div>
@@ -31,8 +34,11 @@ function Tweet({ tweet }: TweetProps) {
         <div className="clickable text-gray-700 hover:bg-green-100 hover:text-green-600">
           <ArrowPathRoundedSquareIcon className="h-5 w-5" />
         </div>
-        <div className="clickable text-gray-700 hover:bg-red-100 hover:text-red-600">
-          <HeartIcon className="h-5 w-5" />
+        <div
+          className="clickable text-gray-700 hover:bg-red-100 hover:text-red-600"
+          onClick={() => setIsLiked(true)}
+        >
+          <HeartIcon className={`h-5 w-5 ${isLiked && "fill-red-400"}`} />
         </div>
         <div className="clickable text-gray-700 hover:text-twitterBlue">
           <ChartBarIcon className="h-5 w-5 hover:text-twitterBlue" />
